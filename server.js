3f6d2965f57a2beed219420d58dda3abed70b0b4
Northcut <mrs.xhief@Mrss-Air.attlocal.net>
2022-02-24 20:58:33 -0500

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
 const path = require('path');
+
+ var filePath = './client/build/index.html';
+ var resolvedPath = path.resolve(filePath);
 
 const PORT = process.env.PORT || 4005;
 const app = express();
@@ -9,15 +11,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, './client/build')))
 
 app.get('/', function(_, res) {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'), function(err) {
+  
+  res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
   })
+
+ console.log(resolvedPath);
+ return res.sendFile(resolvedPath);
 })
+
 
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
