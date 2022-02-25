a9268de521f90e85792a42c86e712ff7f3e92428
Northcut <mrs.xhief@Mrss-Air.attlocal.net>
2022-02-24 20:11:43 -0500

    make it better

--- server.js
+++ server.js
@@ -9,12 +9,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+app.use(express.static(path.join(__dirname, './client/build')))
 
-app.use(express.static(path.resolve(__dirname, '/client/build')));
-
-app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '/client/build/index.html'));
-      });
+app.get('*', function(_, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
