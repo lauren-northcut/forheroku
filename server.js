457c9823e486d190dbc402e8a28fc2c03dbaf667
Northcut <mrs.xhief@Mrss-Air.attlocal.net>
2022-02-24 20:54:44 -0500

    make it better

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'client/build')))
 
-app.get('*', function(_, res) {
+app.get('/', function(_, res) {
   res.sendFile(path.join(__dirname, 'client/build/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
